8b78084869e3f66c494bdc804165e4ed2c1e916c
bjarneo <bjarne.overli@vg.no>
2016-05-08 16:14:52 +0200

    move theme creation to a method

--- src/menu.js
+++ src/menu.js
@@ -5,50 +5,26 @@ const electron = require('electron');
 module.exports = function menu(page) {
     const insertCSS = require('./insert-css')(page);
 
+    function addTheme(label, file) {
+        return {
+            label,
+            file,
+            click() {
+                insertCSS(file);
+
+                page.reload();
+            }
+        };
+    }
+
     const tpl = [
         {
             label: 'Themes',
             submenu: [
-                {
-                    label: 'default',
-                    // Leave it. Will do this automatically
-                    file: 'none',
-                    click() {
-                        insertCSS('none');
-
-                        page.reload();
-                    }
-                },
-                {
-                    label: 'HNU',
-                    // Leave it. Will do this automatically
-                    file: 'hnu.css',
-                    click() {
-                        insertCSS('hnu.css');
-
-                        page.reload();
-                    }
-                },
-                {
-                    label: 'HNU Night',
-                    // Leave it. Will do this automatically
-                    file: 'hnu-night.css',
-                    click() {
-                        insertCSS('hnu-night.css');
-
-                        page.reload();
-                    }
-                },
-                {
-                    label: 'Lost sunset',
-                    // Leave it. Will do this automatically
-                    file: 'lost-sunset.css',
-                    click() {
-                        insertCSS('lost-sunset.css');
-
-                        page.reload();
-                    }
-                }
+                addTheme('default', 'none'),
+                addTheme('HNU', 'hnu.css'),
+                addTheme('HNU Night mode', 'hnu-night.css'),
+                addTheme('Lost sunset', 'lost-sunset.css')
             ]
         },
         {
